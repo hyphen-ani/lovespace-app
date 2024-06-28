01ef0c1e3eae387ec98cc43a54de9c9c366379ab
hyphen-ani <shivammm.hyphen@gmail.com>
2024-06-28 21:36:23 +0530

    Initial Commit

---
+++ lovespace/lovespaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  lovespaceApp.swift
+//  lovespace
+//
+//  Created by Hallucinate Labs on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct lovespaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
